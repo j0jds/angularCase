fea8e2f4ac2d93576ced95beffee5679c3c37da5
j0jds <mrlucassilva7342@gmail.com>
2024-08-06 16:13:17 -0300

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CardComponent } from './card/card.component';
+import { CardRoxoComponent } from './card-roxo/card-roxo.component';
+import { CardButtonComponent } from './card-button/card-button.component';
+import { CardRoxoButtonComponent } from './card-roxo-button/card-roxo-button.component';
+import { CardsComponent } from './cards/cards.component';
+
+
+// Para que um componente seja referenciado em outro, o nome dele deve estar inserido no array declarations de @NgModule.
 
 @NgModule({
   declarations: [
     AppComponent,
-    CardComponent
+    CardComponent,
+    CardRoxoComponent,
+    CardButtonComponent,
+    CardRoxoButtonComponent,
+    CardsComponent
   ],
   imports: [
     BrowserModule,
