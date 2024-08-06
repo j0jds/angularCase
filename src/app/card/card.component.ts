671d712587c2503603febdc0c14c883475c0df5e
j0jds <mrlucassilva7342@gmail.com>
2024-08-06 17:25:25 -0300

    Using get and set methods + setTimeout function

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -8,4 +8,14 @@ import { Component } from '@angular/core';
 export class CardComponent {
   tipo = 'Teste';
   preco = 100;
+
+  getFullPrice() {
+    setTimeout(() => {
+      console.log('Set Timeout');
+      this.tipo = 'Simples';
+    }, 10000);
+
+    return 'R$' + this.preco + ',00/Mês';
+  }
+
 }
