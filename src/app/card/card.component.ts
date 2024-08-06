5a6567dbe54c6ddc693ee30d7b8b9cb0e93ef96b
j0jds <mrlucassilva7342@gmail.com>
2024-08-06 17:53:44 -0300

    Testing optional chaining

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -6,10 +6,6 @@ import { Component } from '@angular/core';
   styleUrl: './card.component.scss'
 })
 export class CardComponent {
-  plano = {
-    infos: {
-      tipo: 'Simples',
-      preco: 100,
-    },
-  };
+  // @ts-ignore
+  plano;
 }
