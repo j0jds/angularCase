ce1d7c20975748aaaebba8c43e4598e4213b9bfb
j0jds <mrlucassilva7342@gmail.com>
2024-08-06 16:11:51 -0300

    Specifics updates of standard card button

---
+++ src/app/card-button/card-button.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'card-button',
+  templateUrl: './card-button.component.html',
+  styleUrl: './card-button.component.scss'
+})
+export class CardButtonComponent {
+
+}
