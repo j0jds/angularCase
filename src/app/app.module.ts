c42796812bc690df2dd69702789cfedd6a1c3cf5
j0jds <mrlucassilva7342@gmail.com>
2024-08-06 17:11:50 -0300

    Testing references of cards.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { CardComponent } from './card/card.component';
-import { CardRoxoComponent } from './card-roxo/card-roxo.component';
-import { CardButtonComponent } from './card-button/card-button.component';
-import { CardRoxoButtonComponent } from './card-roxo-button/card-roxo-button.component';
-import { CardsComponent } from './cards/cards.component';
+import { AppComponent } from './app.component'; 
 
+import { CardsModule } from './cards/cards.module';
 
 // Para que um componente seja referenciado em outro, o nome dele deve estar inserido no array declarations de @NgModule.
 
 @NgModule({
   declarations: [
     AppComponent,
-    CardComponent,
-    CardRoxoComponent,
-    CardButtonComponent,
-    CardRoxoButtonComponent,
-    CardsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    CardsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
