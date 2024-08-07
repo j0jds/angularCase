18ae1659be3c358c4d6de252d7a850cd504c3f1c
j0jds <mrlucassilva7342@gmail.com>
2024-08-07 14:38:22 -0300

    Fixing deleted component references

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component'; 
 
 import { CardsModule } from './cards/cards.module';
-import { InlineComponent } from './inline/inline.component';
 
 // Para que um componente seja referenciado em outro, o nome dele deve estar inserido no array declarations de @NgModule.
 
 @NgModule({
   declarations: [
     AppComponent,
-    InlineComponent,
   ],
   imports: [
     BrowserModule,
