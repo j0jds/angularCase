c417d885b2f993fcb4ac86c8932093a76a5b7fc9
j0jds <mrlucassilva7342@gmail.com>
2024-08-08 16:44:45 -0300

    Clearing

--- project/src/app/app.component.ts
+++ project/src/app/app.component.ts
@@ -6,42 +6,5 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-    inputText = "Texto Inicial Alterado";
-    inputType = "text";
-    isDisabled = true;
-
-  enableInput() {
-
-    this.isDisabled = false;
-
-  }
-
-  disableInput() {
-
-    this.isDisabled = true;
-
-  }
-
-  setPasswordTypeInput() {
-
-    this.inputType = 'password';
-
-  }
-
-  setTextTypeInput() {
-
-    this.inputType = 'text';
-
-  }
-
-  logInputText() {
-    console.log(this.inputText);
-  }
-
-  handleInputKeyup(event: KeyboardEvent) {
-    const currentText = (event.target as HTMLInputElement).value;
-    console.log(currentText);
-
-  }
 
 }
