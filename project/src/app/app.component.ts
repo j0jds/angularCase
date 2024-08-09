805ae7074a487c416803aadfc1e6dbdcb80e5e30
j0jds <mrlucassilva7342@gmail.com>
2024-08-09 16:17:44 -0300

    Updating button function

--- project/src/app/app.component.ts
+++ project/src/app/app.component.ts
@@ -3,19 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   StyleObject = {
-    width: '50px',
+    width: '10px',
   };
 
   updateStyleObject() {
     console.log('updateStyleObject');
+    const currentWidth = parseInt(this.StyleObject.width, 10);
+    const newWidth = currentWidth + 100;
     this.StyleObject = {
-      width: '10px',
-    } 
-   }
+      width: `${newWidth}px`,
+    };
+  }
 }
-
-
