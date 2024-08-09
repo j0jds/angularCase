5ec71a54720fd50def84ba57afe81c972a7c4797
j0jds <mrlucassilva7342@gmail.com>
2024-08-09 16:51:53 -0300

    Clearing app.component.ts

--- project/src/app/app.component.ts
+++ project/src/app/app.component.ts
@@ -6,16 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  StyleObject = {
-    width: '10px',
-  };
+  
+};
 
-  updateStyleObject() {
-    console.log('updateStyleObject');
-    const currentWidth = parseInt(this.StyleObject.width, 10);
-    const newWidth = currentWidth + 100;
-    this.StyleObject = {
-      width: `${newWidth}px`,
-    };
-  }
-}
