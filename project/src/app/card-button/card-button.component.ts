c57d74e0ecb8c0acb3d2e2575c75e2bd6666d687
j0jds <mrlucassilva7342@gmail.com>
2024-08-12 17:58:38 -0300

    Property binding in button test

--- project/src/app/card-button/card-button.component.ts
+++ project/src/app/card-button/card-button.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { booleanAttribute, Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-card-button',
@@ -6,6 +6,10 @@ import { Component, EventEmitter, Output } from '@angular/core';
   styleUrls: ['./card-button.component.scss']
 })
 export class CardButtonComponent {
+
+  propTest: boolean = false;
+
+  @Input({ transform: booleanAttribute }) buttonDisabled: boolean = false;
 
   @Output('buttonClick') buttonClickEmitter = new EventEmitter<boolean>();
 
