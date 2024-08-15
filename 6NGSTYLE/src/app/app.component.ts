9d0da1739eb5718e0dfea4de9fa468151574c11a
j0jds <mrlucassilva7342@gmail.com>
2024-08-15 17:20:58 -0300

    Methods to increase/decrease size + change color

--- 6NGSTYLE/src/app/app.component.ts
+++ 6NGSTYLE/src/app/app.component.ts
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  
+  fontSize: number = 80;
+  textColor: 'green' | 'orange' = 'green';
+  buttonText: 'Verde' | 'Laranja' = 'Laranja';
+
+  increaseFontSize() {
+    this.fontSize = this.fontSize + 10;
+    console.log('fontSize');
+  }
+
+  decreaseFontSize() {
+    this.fontSize = this.fontSize - 20;
+    console.log('ABAIXOU!');
+  }
+
+toggleFontColor() {
+  if (this.textColor === 'green') {
+    this.textColor = 'orange';
+    this.buttonText = 'Verde';
+} else {
+    this.textColor = 'green';
+    this.buttonText = 'Laranja';
+}
+
+}
+
 }
