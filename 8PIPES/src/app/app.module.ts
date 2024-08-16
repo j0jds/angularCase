b9dbb12ecf32471a65c2df2a34ff0cbc16feac5c
j0jds <mrlucassilva7342@gmail.com>
2024-08-16 12:22:37 -0300

    Declaration of StatusClassPipe in app.module.ts

--- 8PIPES/src/app/app.module.ts
+++ 8PIPES/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StatusClassPipe } from './pipes/status-class.pipe'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StatusClassPipe
   ],
   imports: [
     BrowserModule,
