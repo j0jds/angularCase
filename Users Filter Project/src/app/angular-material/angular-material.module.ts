e61e7ba2f0aa8760a7a0d26ab6c2e961e0c75a9d
j0jds <mrlucassilva7342@gmail.com>
2024-08-16 13:39:54 -0300

    Import MatButtonModule

--- Users Filter Project/src/app/angular-material/angular-material.module.ts
+++ Users Filter Project/src/app/angular-material/angular-material.module.ts
@@ -1,11 +1,8 @@
-import { NgModule } from "@angular/core";
+import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-    declarations: [],
-    imports: [],
-    exports: [],
+  imports: [MatButtonModule],
+  exports: [MatButtonModule],
 })
-
-export class AngularMaterialModule {
-     
-}
+export class AngularMaterialModule {}
