fed2bc99fa5a249cce99b34ccfb5276c5497d6d7
j0jds <mrlucassilva7342@gmail.com>
2024-08-19 14:12:36 -0300

    Some imports and exports

--- Users Filter Project/src/app/angular-material/angular-material.module.ts
+++ Users Filter Project/src/app/angular-material/angular-material.module.ts
@@ -1,12 +1,30 @@
 import { NgModule } from '@angular/core';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatListModule } from '@angular/material/list';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {FormsModule} from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
 
 @NgModule({
-  imports: [MatListModule, MatDividerModule, FormsModule, MatFormFieldModule, MatInputModule],
-  exports: [MatListModule, MatDividerModule, FormsModule, MatFormFieldModule, MatInputModule],
+  imports: [
+    MatListModule,
+    MatDividerModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule, 
+  ],
+  exports: [
+    MatListModule,
+    MatDividerModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule, 
+  ],
 })
 export class AngularMaterialModule {}
