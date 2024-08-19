e1d6b744134cb80d452b81cef5f5b048d7c0eb18
j0jds <mrlucassilva7342@gmail.com>
2024-08-19 14:44:41 -0300

    Import/export CommonModule

--- Users Filter Project/src/app/components/components.module.ts
+++ Users Filter Project/src/app/components/components.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { AngularMaterialModule } from '../angular-material/angular-material.module';
 import { UserDetailsComponent } from './user-details/user-details.component';
 import { FilterComponent } from './filter/filter.component';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
-  imports: [AngularMaterialModule],
-  exports: [UserDetailsComponent, FilterComponent],
+  imports: [AngularMaterialModule, CommonModule],
+  exports: [UserDetailsComponent, FilterComponent, CommonModule],
   declarations: [UserDetailsComponent, FilterComponent],
 })
 export class ComponentsModule { 
