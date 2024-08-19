9f983de3178f9d78e17b0804286c24cc9e4a3e9b
j0jds <mrlucassilva7342@gmail.com>
2024-08-19 14:45:01 -0300

    MatSelectModule and more

--- Users Filter Project/src/app/angular-material/angular-material.module.ts
+++ Users Filter Project/src/app/angular-material/angular-material.module.ts
@@ -6,6 +6,8 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { FormsModule } from '@angular/forms';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
+import { MatSelectModule } from '@angular/material/select';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   imports: [
@@ -15,7 +17,9 @@ import { MatNativeDateModule } from '@angular/material/core';
     MatFormFieldModule,
     MatInputModule,
     MatDatepickerModule,
-    MatNativeDateModule, 
+    MatNativeDateModule,
+    MatSelectModule,
+    CommonModule,
   ],
   exports: [
     MatListModule,
@@ -24,7 +28,9 @@ import { MatNativeDateModule } from '@angular/material/core';
     MatFormFieldModule,
     MatInputModule,
     MatDatepickerModule,
-    MatNativeDateModule, 
+    MatNativeDateModule,
+    MatSelectModule,
+    CommonModule,
   ],
 })
 export class AngularMaterialModule {}
