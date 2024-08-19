5e7bef1bd23175a524772a2a48197b297f3226ad
j0jds <mrlucassilva7342@gmail.com>
2024-08-19 15:34:05 -0300

    Importing UsersListComponent

--- Users Filter Project/src/app/components/components.module.ts
+++ Users Filter Project/src/app/components/components.module.ts
@@ -3,11 +3,12 @@ import { AngularMaterialModule } from '../angular-material/angular-material.modu
 import { UserDetailsComponent } from './user-details/user-details.component';
 import { FilterComponent } from './filter/filter.component';
 import { CommonModule } from '@angular/common';
+import { UsersListComponent } from './users-list/users-list.component';
 
 @NgModule({
   imports: [AngularMaterialModule, CommonModule],
-  exports: [UserDetailsComponent, FilterComponent, CommonModule],
-  declarations: [UserDetailsComponent, FilterComponent],
+  exports: [UserDetailsComponent, FilterComponent, CommonModule, UsersListComponent],
+  declarations: [UserDetailsComponent, FilterComponent, UsersListComponent],
 })
 export class ComponentsModule { 
   
