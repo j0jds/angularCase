c07949aefc24a976f2100bb858b93c7b5333da97
j0jds <mrlucassilva7342@gmail.com>
2024-08-19 13:22:15 -0300

    Using more angular material

--- Users Filter Project/src/app/angular-material/angular-material.module.ts
+++ Users Filter Project/src/app/angular-material/angular-material.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatListModule } from '@angular/material/list';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
-  imports: [MatListModule, MatDividerModule],
-  exports: [MatListModule, MatDividerModule],
+  imports: [MatListModule, MatDividerModule, FormsModule, MatFormFieldModule, MatInputModule],
+  exports: [MatListModule, MatDividerModule, FormsModule, MatFormFieldModule, MatInputModule],
 })
 export class AngularMaterialModule {}
