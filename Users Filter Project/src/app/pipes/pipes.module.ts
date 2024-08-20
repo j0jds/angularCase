b71fc331f8fdb8744f76b697f9ca3f91227bb878
j0jds <mrlucassilva7342@gmail.com>
2024-08-20 15:38:11 -0300

    Create data pipe

--- Users Filter Project/src/app/pipes/pipes.module.ts
+++ Users Filter Project/src/app/pipes/pipes.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { PhonePipe } from './phone.pipe';
 import { AddressPipe } from './address.pipe';
 import { StatusPipe } from './status.pipe';
+import { DataPipe } from './data.pipe';
 
 @NgModule({
-  declarations: [PhonePipe, AddressPipe, StatusPipe],
+  declarations: [PhonePipe, AddressPipe, StatusPipe, DataPipe],
   exports: [PhonePipe, AddressPipe, StatusPipe],
 })
 export class PipesModule {}
