01caaedbb40c700c82a710ba1a1cadb80f9b5cfb
j0jds <mrlucassilva7342@gmail.com>
2024-08-20 16:28:34 -0300

    Using dash-if-empty pipe in pipes module

--- Users Filter Project/src/app/pipes/pipes.module.ts
+++ Users Filter Project/src/app/pipes/pipes.module.ts
@@ -3,9 +3,10 @@ import { PhonePipe } from './phone.pipe';
 import { AddressPipe } from './address.pipe';
 import { StatusPipe } from './status.pipe';
 import { DataPipe } from './data.pipe';
+import { DashIfEmptyPipe } from './dash-if-empty.pipe';
 
 @NgModule({
-  declarations: [PhonePipe, AddressPipe, StatusPipe, DataPipe],
-  exports: [PhonePipe, AddressPipe, StatusPipe],
+  declarations: [PhonePipe, AddressPipe, StatusPipe, DataPipe, DashIfEmptyPipe],
+  exports: [PhonePipe, AddressPipe, StatusPipe, DashIfEmptyPipe],
 })
 export class PipesModule {}
