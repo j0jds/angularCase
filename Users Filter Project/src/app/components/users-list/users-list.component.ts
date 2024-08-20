bffa70e040f7c1a122498e681db582cd08177ba1
j0jds <mrlucassilva7342@gmail.com>
2024-08-20 18:24:13 -0300

    Added userSelectedEmitt (wip)

--- Users Filter Project/src/app/components/users-list/users-list.component.ts
+++ Users Filter Project/src/app/components/users-list/users-list.component.ts
@@ -11,8 +11,13 @@ import { UsersList } from '../../data/users-list';
 export class UsersListComponent {
   displayedColumns: string[] = ['name','date','status'];
   usersList: IUser[] = UsersList;
+//Daqui.
+ @Output('userSelected') userSelectedEmitt = new EventEmitter<IUser>();
+// Nesse caso a tipagem do método userSelectedEmitt foi o IUser (a interface de usuário que eu criei anteriormente).
+// Esse já foi um exemplo usado em outras ocasiões, mas achei bom tomar nota disso nesse momento.
 
   onUserSelected(user: IUser) {
     console.log('user', user);
+    this.userSelectedEmitt.emit(user); //Até aqui.
   }
 }
