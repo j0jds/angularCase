6c6ff2a0a9d4507ccd40e90278a578d008bbc019
j0jds <mrlucassilva7342@gmail.com>
2024-09-03 16:38:41 -0300

    Changing setTimeout time

--- UsersFilterProject/src/app/app.component.ts
+++ UsersFilterProject/src/app/app.component.ts
@@ -12,7 +12,7 @@ export class AppComponent implements OnInit {
   ngOnInit() {
     setTimeout(() => {
       this.usersList = UsersList;
-    }, 3000);
+    }, 10);
   }
 
   usersList: IUser[] = [];
