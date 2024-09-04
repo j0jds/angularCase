469177e06eb5404c1f57e0c2d69f556d67fae1a6
j0jds <mrlucassilva7342@gmail.com>
2024-09-04 17:24:52 -0300

    Filter by name working

--- UsersFilterProject/src/app/app.component.ts
+++ UsersFilterProject/src/app/app.component.ts
@@ -6,7 +6,7 @@ import { IFilterOptions } from './interfaces/filter-options.interface';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent implements OnInit {
   usersList: IUser[] = [];
@@ -17,16 +17,38 @@ export class AppComponent implements OnInit {
   ngOnInit() {
     setTimeout(() => {
       this.usersList = UsersList;
+      this.usersListFiltered = this.usersList;
     }, 1);
   }
 
-
   onUserSelected(user: IUser) {
-    this.userSelected =  user;
+    this.userSelected = user;
     this.showUserDetails = true;
   }
 
   onFilter(filterOptions: IFilterOptions) {
     console.log(filterOptions);
+    this.usersListFiltered = this.filterUsersList(
+      filterOptions,
+      this.usersList
+    );
   }
- }
+  filterUsersList(filterOptions: IFilterOptions, usersList: IUser[]): IUser[] {
+    let filteredList: IUser[] = [];
+
+    filteredList = this.filterUsersListByName(filterOptions.name, usersList);
+    return filteredList;
+  }
+  filterUsersListByName(name: string | undefined, usersList: IUser[]): IUser[] {
+    const NAME_NOT_TYPED = name === undefined;
+
+    if (NAME_NOT_TYPED) {
+      return UsersList;
+    }
+    const filteredList = usersList.filter((user) =>
+      user.nome.toLowerCase().includes(name.toLowerCase())
+    );
+
+    return filteredList;
+  }
+}
