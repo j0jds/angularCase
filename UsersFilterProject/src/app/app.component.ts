d3ac288a7f80503005e0f07cc327907f6f77fb7d
j0jds <mrlucassilva7342@gmail.com>
2024-09-04 16:11:40 -0300

    Exhibit filterOptions

--- UsersFilterProject/src/app/app.component.ts
+++ UsersFilterProject/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { IUser } from './interfaces/user/user.interface';
 import { UsersList } from './data/users-list';
+import { IFilterOptions } from './interfaces/filter-options.interface';
 
 @Component({
   selector: 'app-root',
@@ -22,6 +23,10 @@ export class AppComponent implements OnInit {
   onUserSelected(user: IUser) {
     this.userSelected =  user;
     this.showUserDetails = true;
+  }
+
+  onFilter(filterOptions: IFilterOptions) {
+    console.log(filterOptions);
   }
 
  }
