409cb944e5a58bddc4096de407fc57035f7245ae
j0jds <mrlucassilva7342@gmail.com>
2024-09-05 12:31:56 -0300

    Finished filterUsersListByStatus

--- UsersFilterProject/src/app/app.component.ts
+++ UsersFilterProject/src/app/app.component.ts
@@ -39,10 +39,21 @@ export class AppComponent implements OnInit {
 
     filteredList = this.filterUsersListByName(filterOptions.name, usersList);
     filteredList = this.filterUsersListByStatus(filterOptions.status, filteredList);
+    
     return filteredList;
   }
-  filterUsersListByStatus(status: boolean | undefined, filteredList: IUser[]): IUser[] {
-    const 
+
+  filterUsersListByStatus(status: boolean | undefined, usersList: IUser[]): IUser[] {
+    const STATUS_NOT_SELECTED = status === undefined;
+    
+    if(STATUS_NOT_SELECTED) {
+      return this.usersList;
+    }
+
+    const filteredList = usersList.filter((user) => user.ativo === status);
+
+    return filteredList;
+
   }
 
   filterUsersListByName(name: string | undefined, usersList: IUser[]): IUser[] {
@@ -51,9 +62,11 @@ export class AppComponent implements OnInit {
     if (NAME_NOT_TYPED) {
       return UsersList;
     }
+
     const filteredList = usersList.filter((user) =>
       user.nome.toLowerCase().includes(name.toLowerCase())
     );
+    
     return filteredList;
   }
 
