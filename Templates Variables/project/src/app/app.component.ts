3bec1a21998ab9a68f0aae961a71adfea004c0a4
j0jds <mrlucassilva7342@gmail.com>
2024-09-06 16:55:55 -0300

    Added button and console function

--- Templates Variables/project/src/app/app.component.ts
+++ Templates Variables/project/src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  teste = '';
+  clicou(input: any) {
+    console.log(input.value);
+  }
 }
