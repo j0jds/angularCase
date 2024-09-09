90b17a8cea499b2d2bb97b37cda7d44dab65a78c
j0jds <mrlucassilva7342@gmail.com>
2024-09-09 14:18:09 -0300

    Added ElementRef for HTMLDivELement and updateDivContent method

--- Decorators/ViewChild/src/app/app.component.ts
+++ Decorators/ViewChild/src/app/app.component.ts
@@ -3,12 +3,15 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   
   @ViewChild('meuInput') 
-  meuInputEl!: ElementRef<HTMLInputElement> 
+  meuInputEl!: ElementRef<HTMLInputElement> ;
+
+  @ViewChild('minhaDiv')
+  minhaDivEl!: ElementRef<HTMLDivElement>;
   
   updateInputText () {
     console.log(this.meuInputEl);
@@ -19,4 +22,7 @@ export class AppComponent {
     this.meuInputEl.nativeElement.focus();
   }
 
+  updateDivContent() {
+    this.minhaDivEl.nativeElement.textContent = 'Conteúdo Atualizado!!!';
+  }
 }
