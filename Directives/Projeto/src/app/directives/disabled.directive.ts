0f56bc00df7d5a2d9c093e83fb7b9a75abc306a0
j0jds <mrlucassilva7342@gmail.com>
2024-09-13 17:56:04 -0300

    Added new HostBinding

--- Directives/Projeto/src/app/directives/disabled.directive.ts
+++ Directives/Projeto/src/app/directives/disabled.directive.ts
@@ -1,10 +1,9 @@
-import { Directive } from '@angular/core';
+import { Directive, HostBinding } from '@angular/core';
 
 @Directive({
   selector: '[appDisabled]'
 })
 export class DisabledDirective {
-
-  constructor() { }
-
+  // @HostBinding('attr.disabled') attrDisabled = false;
+  @HostBinding('disabled') propDisabled = true;
 }
