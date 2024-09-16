93ad2abf59d632d89246f49a93694eed06cdd1b9
j0jds <mrlucassilva7342@gmail.com>
2024-09-16 15:44:20 -0300

    Changing text and background color by input

--- Directives/Projeto/src/app/directives/input-background.directive.ts
+++ Directives/Projeto/src/app/directives/input-background.directive.ts
@@ -6,16 +6,20 @@ import { Directive, HostBinding, HostListener, Input } from "@angular/core";
 
 export class InputBackgroundDirective {
     @Input() appInputBackground: string = 'white';
+    @Input() textColor: string = 'black';
 
     @HostBinding('style.backgroundColor') bgColor: string = '';
+    @HostBinding('style.color') color: string = '';
 
     @HostListener('focus')
     onFocus() {
         this.bgColor = this.appInputBackground;
+        this.color = this.textColor;
     }
 
     @HostListener('blur')
     onBlur(){
         this.bgColor = 'white';
+        this.color = 'black';
     }
 }
