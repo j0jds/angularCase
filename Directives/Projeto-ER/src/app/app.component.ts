4cd8742fa5d47e08a2d22cba2f2980ce5a95f983
j0jds <mrlucassilva7342@gmail.com>
2024-09-16 17:33:23 -0300

    Using nativeElement access, constructor and more

--- Directives/Projeto-ER/src/app/app.component.ts
+++ Directives/Projeto-ER/src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'Projeto-ER';
+export class AppComponent implements OnInit, AfterViewInit {
+  @ViewChild('minhaDiv') divEl!: ElementRef<HTMLDivElement>;
+
+  constructor(private readonly _elRef: ElementRef) {
+    this._elRef
+  }
+
+  ngOnInit() {
+      console.log(this._elRef);
+  }
+
+  ngAfterViewInit() {
+    this.divEl.nativeElement.style.backgroundColor = 'orange';
+    this.divEl.nativeElement.textContent = 'Sou uma div!';
+    this.divEl.nativeElement.classList.add('minha-classe');
+  }
 }
