97c32936d5e9bbc3aa3bf264a56e1271c459d012
j0jds <mrlucassilva7342@gmail.com>
2024-09-17 15:51:06 -0300

    Using renderer2 to prevent XSS Attacks

--- Directives/Projeto-ER/src/app/xss/xss.component.ts
+++ Directives/Projeto-ER/src/app/xss/xss.component.ts
@@ -1,22 +1,31 @@
-import { Component, ElementRef } from '@angular/core';
+import { Component, ElementRef, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-xss',
   templateUrl: './xss.component.html',
-  styleUrl: './xss.component.scss'
+  styleUrl: './xss.component.scss',
 })
 export class XSSComponent {
-
-  constructor(private readonly _elRef: ElementRef) {}
+  constructor(
+    private readonly _elRef: ElementRef,
+    private readonly _renderer2: Renderer2
+  ) {}
 
   createElement(inputText: string) {
-    
     const divEl = document.createElement('div');
-  
-    divEl.innerHTML = inputText;
-  
-    this._elRef.nativeElement.appendChild(divEl); 
 
+    divEl.innerHTML = inputText;
+
+    this._elRef.nativeElement.appendChild(divEl);
   }
 
+  createElementCorrect(inputText: string) {
+    const divEl = this._renderer2.createElement('div');
+
+    const text = this._renderer2.createText(inputText);
+
+    this._renderer2.appendChild(divEl, text);
+
+    this._renderer2.appendChild(this._elRef.nativeElement, divEl);
+  }
 }
