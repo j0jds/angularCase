f6c08411e9f4536d4a3d95737d64c4e46f60b6bc
j0jds <mrlucassilva7342@gmail.com>
2024-09-17 14:21:47 -0300

    General update

--- Directives/Projeto-ER/src/app/directives/disabled.directive.ts
+++
@@ -1,9 +0,0 @@
-import { Directive, HostBinding } from '@angular/core';
-
-@Directive({
-  selector: '[appDisabled]'
-})
-export class DisabledDirective {
-  // @HostBinding('attr.disabled') attrDisabled = false;
-  @HostBinding('disabled') propDisabled = true;
-}
