01e9530c8e4326d3630718bff719b3567e97d4b6
j0jds <mrlucassilva7342@gmail.com>
2024-09-17 13:38:48 -0300

    Importing directives

--- Directives/Projeto-ER/src/app/app.module.ts
+++ Directives/Projeto-ER/src/app/app.module.ts
@@ -1,13 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HighlightDirective } from '../app/directives/highlight.directive';
+import { DisabledDirective } from '../app/directives/disabled.directive';
+import { StyleDirective } from './directives/style.directive';
+import { ClassDirective } from './directives/class.directive';
+import { ListenerDirective } from './directives/listener.directive';
+import { InputBackgroundDirective } from './directives/input-background.directive';
+import { FocusSecondInput } from './directives/focus-second-input.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    HighlightDirective,
+    DisabledDirective,
+    StyleDirective, 
+    ClassDirective,
+    ListenerDirective,
+    InputBackgroundDirective,
+    FocusSecondInput
+    ],
   imports: [
     BrowserModule,
     AppRoutingModule
