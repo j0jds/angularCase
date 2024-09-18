2008816c036921258af1a3a65a1d2bddf742465e
j0jds <mrlucassilva7342@gmail.com>
2024-09-18 17:21:31 -0300

    Logic to dynamize body text (wip)

--- Projects/Cards Panel/Modern/src/app/components/card-body/card-body.component.ts
+++ Projects/Cards Panel/Modern/src/app/components/card-body/card-body.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card-body',
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrl: './card-body.component.scss'
 })
 export class CardBodyComponent {
+
+// @Input({ required: true })
+// cardSaldos: string = '';
+
+// @Input({ required: true })
+// cardCredit: string = '';
+
+// @Input({ required: true })
+// cardDebit: string = '';
+
+// @Input({ required: true })
+// cardClient: string = '';
 
 }
