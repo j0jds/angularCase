343d306e96f8a3b9a92b3f28cd056c293365c105
j0jds <mrlucassilva7342@gmail.com>
2024-09-18 17:51:18 -0300

    Using data (wip)

---
+++ Projects/Cards Panel/Modern/src/app/data/user-data.ts
@@ -0,0 +1,12 @@
+// import { ISaldos } from "../interfaces/saldos";
+
+// export const ClientList: ISaldos[] = [
+//     {
+//         corrente: 200,
+//         vinculados: 300,
+//         poupanca: 400,
+//         investimentoscb: 500,
+//         investimentossb: 500,
+
+//     }
+// ];
