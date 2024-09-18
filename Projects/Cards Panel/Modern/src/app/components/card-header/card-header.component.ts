0611587fe9326ad640b6c9998c265458d5fcb904
j0jds <mrlucassilva7342@gmail.com>
2024-09-18 17:22:19 -0300

    Importing in app component the header and the body

--- Projects/Cards Panel/Modern/src/app/components/card-header/card-header.component.ts
+++ Projects/Cards Panel/Modern/src/app/components/card-header/card-header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card-header',
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrl: './card-header.component.scss'
 })
 export class CardHeaderComponent {
+
+@Input({ required: true })
+headerType: string = '';
+
+@Input({ required: true })
+headerStyle: 'orange' | 'cyan' = 'orange';
 
 }
