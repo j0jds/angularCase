3bc1b84b28a5253b84cd880a825b547fd25f1650
j0jds <mrlucassilva7342@gmail.com>
2024-09-19 12:58:31 -0300

    Importing components

--- Projects/Cards Panel/Modern/src/app/app.module.ts
+++ Projects/Cards Panel/Modern/src/app/app.module.ts
@@ -1,20 +1,17 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CardHeaderComponent } from './components/card-header/card-header.component';
-import { CardBodyComponent } from './components/card-body/card-body.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { ComponentsModule } from './components/components.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CardHeaderComponent,
-    CardBodyComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ComponentsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
