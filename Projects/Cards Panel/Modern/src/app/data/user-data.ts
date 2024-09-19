a910df33677f3e9473f80be242d44c236128b308
j0jds <mrlucassilva7342@gmail.com>
2024-09-19 12:56:41 -0300

    Assigning values ​​to the interface

--- Projects/Cards Panel/Modern/src/app/data/user-data.ts
+++ Projects/Cards Panel/Modern/src/app/data/user-data.ts
@@ -1,12 +1,26 @@
-// import { ISaldos } from "../interfaces/saldos";
+import { ICliente } from '../interfaces/cliente';
 
-// export const ClientList: ISaldos[] = [
-//     {
-//         corrente: 200,
-//         vinculados: 300,
-//         poupanca: 400,
-//         investimentoscb: 500,
-//         investimentossb: 500,
-
-//     }
-// ];
+export const ClientList: ICliente[] = [
+  {
+    nome: 'Joel Santana',
+    idade: 75,
+    rendaMensal: 1000,
+    nacionalidade: 'brasileiro',
+    saldos: {
+      corrente: 200,
+      vinculados: 300,
+      poupanca: 400,
+      investimentoscb: 500,
+      investimentossb: 500,
+      total: 1000,
+      livre: 1000,
+    },
+    credito: {
+      limite: 1000,
+      disponivel: 1200,
+      limiteSaque: 2000,
+      disponivel2: 1300,
+      limiteTotalUtilizado: 3000,
+    },
+  },
+];
