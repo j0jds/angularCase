fee42dfa9c0f8da6f0d036f8aa6294684ba21e76
j0jds <mrlucassilva7342@gmail.com>
2024-09-23 14:25:30 -0300

    Using text description directive

--- Projects/Modifiers and Selectors/src/app/app.component.ts
+++ Projects/Modifiers and Selectors/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CardTextTitleDirective } from "./components/cards/directives/card-text-title.directive";
 import { CardTextSubtitleDirective } from "./components/cards/directives/card-text-subtitle.directive";
+import { CardTextDescriptionDirective } from "./components/cards/directives/card-text-description.directive";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CardTextTitleDirective, CardTextSubtitleDirective],
+  imports: [RouterOutlet, CardTextTitleDirective, CardTextSubtitleDirective, CardTextDescriptionDirective],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
