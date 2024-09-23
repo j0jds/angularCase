21b54e27c131c0c2502c4423916737ff4f5a95d5
j0jds <mrlucassilva7342@gmail.com>
2024-09-23 17:12:01 -0300

    Create main text directive

---
+++ Projects/Projection + Directives/src/app/card/directives/base/texts/card-main-text.directive.ts
@@ -0,0 +1,8 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: 'app-card-main-text',
+  host: {'class': 'ca-c-card__main-text'}
+})
+
+export class CardMainTextDirective { }
