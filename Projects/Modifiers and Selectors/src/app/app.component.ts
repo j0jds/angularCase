cabca9aad731ea22b9841c89316e13a4a28b86e3
j0jds <mrlucassilva7342@gmail.com>
2024-09-23 14:22:04 -0300

    Using text title directive

--- Projects/Modifiers and Selectors/src/app/app.component.ts
+++ Projects/Modifiers and Selectors/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CardTextTitleDirective } from "./components/cards/directives/card-text-title.directive";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CardTextTitleDirective],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
