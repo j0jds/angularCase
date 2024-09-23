65ab8a1cf2dc017e627292c493ed5c792fcb2cf5
j0jds <mrlucassilva7342@gmail.com>
2024-09-23 15:18:45 -0300

    Importing avatar small directive in app.component.ts

--- Projects/Modifiers and Selectors/src/app/app.component.ts
+++ Projects/Modifiers and Selectors/src/app/app.component.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { CardTextTitleDirective } from "./components/cards/directives/card-text-title.directive";
-import { CardTextSubtitleDirective } from "./components/cards/directives/card-text-subtitle.directive";
-import { CardTextDescriptionDirective } from "./components/cards/directives/card-text-description.directive";
+import { CardTextTitleDirective } from './components/cards/directives/card-text-title.directive';
+import { CardAvatarSmallDirective } from './components/cards/directives/card-avatar-small.directive';
+import { CardTextSubtitleDirective } from './components/cards/directives/card-text-subtitle.directive';
+import { CardTextDescriptionDirective } from './components/cards/directives/card-text-description.directive';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CardTextTitleDirective, CardTextSubtitleDirective, CardTextDescriptionDirective],
+  imports: [
+    RouterOutlet,
+    CardTextTitleDirective,
+    CardAvatarSmallDirective,
+    CardTextSubtitleDirective,
+    CardTextDescriptionDirective,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent { }
+
+export class AppComponent {}
