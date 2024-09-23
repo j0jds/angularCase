e7969be37733875366073aec69b4b509488a0b10
j0jds <mrlucassilva7342@gmail.com>
2024-09-23 13:40:28 -0300

    Changing imports

--- Projects/Projection/src/app/app.module.ts
+++ Projects/Projection/src/app/app.module.ts
@@ -1,9 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CardComponent } from '../app/components/card/card.component';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
   declarations: [
@@ -11,8 +10,7 @@ import { CardComponent } from '../app/components/card/card.component';
     CardComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
