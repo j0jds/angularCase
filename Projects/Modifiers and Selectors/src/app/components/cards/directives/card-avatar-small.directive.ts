859f6dcb87950a88695513c470c6aa1549e33700
j0jds <mrlucassilva7342@gmail.com>
2024-09-23 15:19:01 -0300

    Using avatar small directive

--- Projects/Modifiers and Selectors/src/app/components/cards/directives/card-avatar-small.directive.ts
+++ Projects/Modifiers and Selectors/src/app/components/cards/directives/card-avatar-small.directive.ts
@@ -2,7 +2,8 @@ import { Directive } from '@angular/core';
 
 @Directive({
   selector: 'app-card-avatar-small',
-  standalone: true
+  standalone: true,
+  host: {'class': 'ca-u-card-avatar-small'}
 })
 
 export class CardAvatarSmallDirective { }
