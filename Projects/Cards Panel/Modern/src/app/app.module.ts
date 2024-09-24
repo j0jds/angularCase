5f6a40e583d9554bd8a94b4530e901c3e0948aff
j0jds <mrlucassilva7342@gmail.com>
2024-09-24 13:02:37 -0300

    Create card content and header directives

--- Projects/Cards Panel/Modern/src/app/app.module.ts
+++ Projects/Cards Panel/Modern/src/app/app.module.ts
@@ -1,25 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-// ---------------------------------------------------------------------------------------------------//
+// --------------------------------------------------------------------------------------------------- //
 import { AppComponent } from './app.component';
+import { CardComponent } from './components/card/card.component';
 import { CardItemComponent } from './components/card-item/card-item.component';
 import { CardLineComponent } from './components/card-line/card-line.component';
+import { CardTitleComponent } from './components/card-title/card-title.component';
 import { DebitCardComponent } from './components/debit-card/debit-card.component';
 import { CreditCardComponent } from './components/credit-card/credit-card.component';
+import { CardHeaderDirective } from './components/card/directives/card-header.directive';
 import { BalanceCardComponent } from './components/balance-card/balance-card.component';
+import { CardContentDirective } from './components/card/directives/card-content.directive';
 import { ClientInfosCardComponent } from './components/client-infos-card/client-infos-card.component';
-import { CardTitleComponent } from './components/card-title/card-title.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    CardComponent,
     CardItemComponent,
     CardLineComponent,
+    CardTitleComponent,
     DebitCardComponent,
     CreditCardComponent,
+    CardHeaderDirective,
     BalanceCardComponent,
+    CardContentDirective,
     ClientInfosCardComponent,
-    CardTitleComponent,
   ],
   imports: [
     BrowserModule
@@ -27,4 +33,5 @@ import { CardTitleComponent } from './components/card-title/card-title.component
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
