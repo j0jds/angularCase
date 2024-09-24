ea9d577322be595fea986fcb7f32c7b98dfcc31b
j0jds <mrlucassilva7342@gmail.com>
2024-09-24 17:52:57 -0300

    General update (wip)

--- Projects/NGCvsNGT/src/app/app.module.ts
+++ Projects/NGCvsNGT/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { MeuComponent } from './meu/meu.component';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MeuComponent
+    MeuComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
