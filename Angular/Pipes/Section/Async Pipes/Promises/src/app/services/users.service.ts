61f8095d53517845c14e92c17686b56b07d10a4b
j0jds <mrlucassilva7342@gmail.com>
2024-09-27 17:51:41 -0300

    Create IUSer and UsersService (wip)

---
+++ Angular/Pipes/Section/Async Pipes/Promises/src/app/services/users.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { IUser } from '../interfaces/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class UsersService {
+
+  getUser() {
+    return new Promise<IUser>((resolve, reject) => {
+      setTimeout(() => resolve({
+        id: 1,
+        name: "Leanne Graham",
+        username: "Bret",
+        email: "Sincere@april.biz",
+      }), 3000);
+    })
+  }
+
+}
