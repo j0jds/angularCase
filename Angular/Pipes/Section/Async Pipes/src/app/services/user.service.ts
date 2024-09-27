d9f5066e65d012677a9715d184b2001356135ec1
j0jds <mrlucassilva7342@gmail.com>
2024-09-27 13:57:29 -0300

    Create service for Http requisition

--- Angular/Pipes/Section/Async Pipes/src/app/services/user.service.ts
+++ Angular/Pipes/Section/Async Pipes/src/app/services/user.service.ts
@@ -1,9 +1,21 @@
+import { Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { IUserResponse } from '../interfaces/user-response';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class UserService {
 
-  constructor() { }
+  constructor(
+    private readonly _http: HttpClient
+  ) { }
+
+  getUserById(userId: number): Observable<IUserResponse> {
+    return this._http.get<IUserResponse>(`https://jsonplaceholder.typicode.com/users/${userId}`);
+  }
+
 }
+ 
