ddbd34a31b831192cc7ccfefd276312a25c6485e
j0jds <mrlucassilva7342@gmail.com>
2024-09-27 13:03:58 -0300

    Create new component

---
+++ Angular/Pipes/Section/Async Pipes/src/app/components/obs-com-objeto/obs-com-objeto.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-obs-com-objeto',
+  templateUrl: './obs-com-objeto.component.html',
+  styleUrl: './obs-com-objeto.component.scss'
+})
+
+export class ObsComObjetoComponent {
+
+}
