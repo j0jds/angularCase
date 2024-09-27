cc2caad1eeabb88d8330be89cb1159408d4f4a1a
j0jds <mrlucassilva7342@gmail.com>
2024-09-27 13:53:59 -0300

    Added HttpClientModule

--- Angular/Pipes/Section/Async Pipes/src/app/app.module.ts
+++ Angular/Pipes/Section/Async Pipes/src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 import { ObsComObjetoComponent } from './components/obs-com-objeto/obs-com-objeto.component';
 
 @NgModule({
@@ -12,7 +14,8 @@ import { ObsComObjetoComponent } from './components/obs-com-objeto/obs-com-objet
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
