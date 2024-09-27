820a827623a8eee3fa5467d9f2ad929fed034ba4
j0jds <mrlucassilva7342@gmail.com>
2024-09-27 16:04:15 -0300

    Create new component

--- Angular/Pipes/Section/Async Pipes/src/app/app.module.ts
+++ Angular/Pipes/Section/Async Pipes/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
+import { ObsComArrayComponent } from './components/obs-com-array/obs-com-array.component';
 import { ObsComObjetoComponent } from './components/obs-com-objeto/obs-com-objeto.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    ObsComArrayComponent,
     ObsComObjetoComponent
   ],
   imports: [
---
+++ Angular/Pipes/Section/Async Pipes/src/app/components/obs-com-array/obs-com-array.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-obs-com-array',
+  templateUrl: './obs-com-array.component.html',
+  styleUrl: './obs-com-array.component.scss'
+})
+export class ObsComArrayComponent {
+
+}
