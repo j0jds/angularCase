4af02371efd0d7025fffb0207244cd3bc1ec89a5
j0jds <mrlucassilva7342@gmail.com>
2024-09-27 15:50:38 -0300

    Using subscribe with Async Pipe

--- Angular/Pipes/Section/Async Pipes/src/app/components/obs-com-objeto/obs-com-objeto.component.ts
+++ Angular/Pipes/Section/Async Pipes/src/app/components/obs-com-objeto/obs-com-objeto.component.ts
@@ -1,4 +1,4 @@
-import { Subscription } from 'rxjs'
+import { Observable, Subscription } from 'rxjs'
 import { UserService } from '../../services/user.service';
 import { IUserResponse } from '../../interfaces/user-response';
 import { Component, OnInit } from '@angular/core';
@@ -13,10 +13,12 @@ export class ObsComObjetoComponent implements OnInit {
 
   user: IUserResponse = {} as IUserResponse; 
   userSubs: Subscription | undefined; 
+  user$!: Observable<IUserResponse>;
 
   constructor(private readonly _userService: UserService) {}
 
   ngOnInit() {
+    this.user$ = this._userService.getUserById(9);
     this.userSubs = this._userService.getUserById(1).subscribe((UserResponse) =>  this.user = UserResponse);
   }
 
