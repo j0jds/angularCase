2033079b09250379f8a926f8dec95a25174d5dd8
j0jds <mrlucassilva7342@gmail.com>
2024-09-27 16:26:10 -0300

    Definition of users$ with UsersListResponse

--- Angular/Pipes/Section/Async Pipes/src/app/components/obs-com-array/obs-com-array.component.ts
+++ Angular/Pipes/Section/Async Pipes/src/app/components/obs-com-array/obs-com-array.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { UserService } from '../../services/user.service';
+import { Component, OnInit } from '@angular/core';
+import { UsersListResponse } from '../../types/users-list-response.type';
 
 @Component({
   selector: 'app-obs-com-array',
   templateUrl: './obs-com-array.component.html',
   styleUrl: './obs-com-array.component.scss'
 })
-export class ObsComArrayComponent {
 
+export class ObsComArrayComponent implements OnInit {
+
+  users$!: Observable<UsersListResponse>;
+
+  constructor(
+    private readonly _userService: UserService
+  ) {}
+
+  ngOnInit() {
+    this.users$ = this._userService.getUsers();
+  }
+  
 }
