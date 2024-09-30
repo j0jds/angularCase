d7f2b87b13c34b2b06761d101f30cfb492848db2
j0jds <mrlucassilva7342@gmail.com>
2024-09-30 15:58:36 -0300

    Added date pipe and specifications for location

--- Projects/Pipes Challenge/Base/src/app/app.module.ts
+++ Projects/Pipes Challenge/Base/src/app/app.module.ts
@@ -1,20 +1,24 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { StatusPipe } from './pipes/status.pipe';
 import { StatusIconPipe } from './pipes/status-icon.pipe';
 
+import localePt from '@angular/common/locales/pt';
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData(localePt, 'pt-BR');
+
 @NgModule({
-  declarations: [
-    AppComponent,
-    StatusPipe,
-    StatusIconPipe
+  declarations: [AppComponent, StatusPipe, StatusIconPipe],
+  imports: [BrowserModule],
+  providers: [
+    {
+      provide: LOCALE_ID,
+      useValue: 'pt-BR',
+    },
   ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
