c5384941f1582a535ff5fd7181f4ea875c2f327b
j0jds <mrlucassilva7342@gmail.com>
2024-09-30 13:28:24 -0300

    Added HttpClient and HttpClientModule

--- Angular/Pipes/Section/Async Pipes/Promises/src/app/services/users.service.ts
+++ Angular/Pipes/Section/Async Pipes/Promises/src/app/services/users.service.ts
@@ -1,11 +1,17 @@
-import { Injectable } from '@angular/core';
 import { IUser } from '../interfaces/user.interface';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class UsersService {
+  constructor(
+    private readonly _http: HttpClient
+  ) {}
+
 
   getUser() {
     return new Promise<IUser>((resolve, reject) => {
@@ -15,7 +21,11 @@ export class UsersService {
         username: "Bret",
         email: "Sincere@april.biz",
       }), 3000);
-    })
+    });
+  }
+
+  getUserById(userId: number): Observable<IUser> {
+    return this._http.get<IUser>(`https://jsonplaceholder.typicode.com/users/${userId}`);
   }
 
 }
