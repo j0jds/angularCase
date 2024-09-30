479762f4ac8ce34f1a702941d79f3b0c69209398
j0jds <mrlucassilva7342@gmail.com>
2024-09-30 16:03:11 -0300

    Added currency pipe with specifications

--- Projects/Pipes Challenge/Base/src/app/app.module.ts
+++ Projects/Pipes Challenge/Base/src/app/app.module.ts
@@ -1,11 +1,9 @@
-import { LOCALE_ID, NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule } from '@angular/core';
+import localePt from '@angular/common/locales/pt';
 import { StatusPipe } from './pipes/status.pipe';
 import { StatusIconPipe } from './pipes/status-icon.pipe';
-
-import localePt from '@angular/common/locales/pt';
 import { registerLocaleData } from '@angular/common';
 
 registerLocaleData(localePt, 'pt-BR');
@@ -18,7 +16,12 @@ registerLocaleData(localePt, 'pt-BR');
       provide: LOCALE_ID,
       useValue: 'pt-BR',
     },
+    {
+      provide: DEFAULT_CURRENCY_CODE,
+      useValue: 'BRL',
+    },
   ],
   bootstrap: [AppComponent],
 })
+
 export class AppModule {}
