4cbd31766faf3681171c477b9654df0425ca4ed9
j0jds <mrlucassilva7342@gmail.com>
2024-10-01 13:28:44 -0300

    Comparisons between instances

--- Angular/Templates/Template Driven-forms/Driven-Forms/src/app/app.component.ts
+++ Angular/Templates/Template Driven-forms/Driven-Forms/src/app/app.component.ts
@@ -1,21 +1,24 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
+export class AppComponent implements AfterViewInit {
+  @ViewChild('meuInputFormControl') inputElFormControl!: NgModel;
+  @ViewChild('meuInputFormControl') inputEl!: ElementRef<HTMLInputElement>;
 
-export class AppComponent { 
-  nome: string = 'Joel';
-
-  onChange(text: string) {
-    console.log(text);
-
+  ngAfterViewInit() {
+    console.log('Form Control =>', this.inputElFormControl);
+    console.log('Original =>', this.inputEl);
   }
 
-  show() {
-    console.log(this.nome)
+  send() {
+    if(this.inputElFormControl.valid && this.inputElFormControl.touched) {
+      console.log('Enviado com sucesso!');
+    }
   }
 
 }
