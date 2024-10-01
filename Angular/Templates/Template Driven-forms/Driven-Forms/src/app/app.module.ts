d3206b8d7a167379721ef57ed601c4cc46ea75d4
j0jds <mrlucassilva7342@gmail.com>
2024-10-01 17:39:12 -0300

    Create and use checkbox component with ngModel

--- Angular/Templates/Template Driven-forms/Driven-Forms/src/app/app.module.ts
+++ Angular/Templates/Template Driven-forms/Driven-Forms/src/app/app.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { SelectComponent } from './elements/select/select.component';
 import { AppRoutingModule } from './app-routing.module';
+import { CheckboxComponent } from './elements/checkbox/checkbox.component';
 import { TextareaComponent } from './elements/textarea/textarea/textarea.component';
 import { InputTextComponent } from './elements/input-text/input-text.component';
-import { SelectComponent } from './elements/select/select.component';
 import { RadioButtonComponent } from './elements/radio-button/radio-button.component';
 
 @NgModule({
-  declarations: [AppComponent, TextareaComponent, InputTextComponent, SelectComponent, RadioButtonComponent],
+  declarations: [AppComponent, TextareaComponent, InputTextComponent, SelectComponent, RadioButtonComponent, CheckboxComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
