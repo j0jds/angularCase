1d56055a9adbf5844cad9955dbeebdbea93765bd
j0jds <mrlucassilva7342@gmail.com>
2024-10-01 14:42:02 -0300

    Create select component

--- Angular/Templates/Template Driven-forms/Driven-Forms/src/app/app.module.ts
+++ Angular/Templates/Template Driven-forms/Driven-Forms/src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { TextareaComponent } from './elements/textarea/textarea/textarea.component';
 import { InputTextComponent } from './elements/input-text/input-text.component';
+import { SelectComponent } from './elements/select/select/select.component';
 
 @NgModule({
-  declarations: [AppComponent, TextareaComponent, InputTextComponent],
+  declarations: [AppComponent, TextareaComponent, InputTextComponent, SelectComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ Angular/Templates/Template Driven-forms/Driven-Forms/src/app/elements/select/select/select.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-select',
+  templateUrl: './select.component.html',
+  styleUrl: './select.component.scss'
+})
+
+export class SelectComponent {
+
+}
