a75e6aa3e3b95b45ba5a2b04037417067a8b8dc3
j0jds <mrlucassilva7342@gmail.com>
2024-10-01 14:40:56 -0300

    Testing textarea component with ngModel

--- Angular/Templates/Template Driven-forms/Driven-Forms/src/app/elements/textarea/textarea/textarea.component.ts
+++ Angular/Templates/Template Driven-forms/Driven-Forms/src/app/elements/textarea/textarea/textarea.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-textarea',
   templateUrl: './textarea.component.html',
-  styleUrl: './textarea.component.scss'
+  styleUrl: './textarea.component.scss',
 })
-export class TextareaComponent {
 
+export class TextareaComponent {
+  text: string = 'Sou o texto inicial';
+
+  onChange(text: string) {
+    this.text = text;
+  }
+
+  showText() {
+    console.log(this.text);
+  }
 }
