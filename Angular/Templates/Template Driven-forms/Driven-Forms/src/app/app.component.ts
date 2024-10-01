0673f7aadf66f3aa71d99b88dd33b8f9283f1ae4
j0jds <mrlucassilva7342@gmail.com>
2024-10-01 14:22:01 -0300

    Update path and files of input

--- Angular/Templates/Template Driven-forms/Driven-Forms/src/app/app.component.ts
+++ Angular/Templates/Template Driven-forms/Driven-Forms/src/app/app.component.ts
@@ -1,24 +1,9 @@
-import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
-import { NgModel } from '@angular/forms';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent implements AfterViewInit {
-  @ViewChild('meuInputFormControl') inputElFormControl!: NgModel;
-  @ViewChild('meuInputFormControl') inputEl!: ElementRef<HTMLInputElement>;
 
-  ngAfterViewInit() {
-    console.log('Form Control =>', this.inputElFormControl);
-    console.log('Original =>', this.inputEl);
-  }
-
-  send() {
-    if(this.inputElFormControl.valid && this.inputElFormControl.touched) {
-      console.log('Enviado com sucesso!');
-    }
-  }
-
-}
+export class AppComponent {}
