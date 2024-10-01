3f6133c4492fcbee03621192b10b14dade1bffe7
j0jds <mrlucassilva7342@gmail.com>
2024-10-01 14:22:38 -0300

    Create textarea component and update imports

--- Angular/Templates/Template Driven-forms/Driven-Forms/src/app/app.module.ts
+++ Angular/Templates/Template Driven-forms/Driven-Forms/src/app/app.module.ts
@@ -1,21 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { TextareaComponent } from './elements/textarea/textarea/textarea.component';
+import { InputTextComponent } from './elements/input-text/input-text.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule,
-  ],
+  declarations: [AppComponent, TextareaComponent, InputTextComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 
 export class AppModule {}
