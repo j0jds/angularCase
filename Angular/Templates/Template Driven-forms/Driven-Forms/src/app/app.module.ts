a06be8c4407cb20b5fad01359594e1952130bfda
j0jds <mrlucassilva7342@gmail.com>
2024-10-02 17:57:42 -0300

    General update (wip)

--- Angular/Templates/Template Driven-forms/Driven-Forms/src/app/app.module.ts
+++ Angular/Templates/Template Driven-forms/Driven-Forms/src/app/app.module.ts
@@ -6,13 +6,13 @@ import { FormComponent } from './elements/form/form.component';
 import { SelectComponent } from './elements/select/select.component';
 import { AppRoutingModule } from './app-routing.module';
 import { CheckboxComponent } from './elements/checkbox/checkbox.component';
-import { FormUserComponent } from './elements/form-user/form-user.component';
-import { MainFormComponent } from './elements/main-form/main-form.component';
+import { FormUserComponent } from './elements/form-with-children/form-user/form-user.component';
+import { MainFormComponent } from './elements/form-with-children/main-form/main-form.component';
 import { TextareaComponent } from './elements/textarea/textarea.component';
 import { InputTextComponent } from './elements/input-text/input-text.component';
 import { RadioButtonComponent } from './elements/radio-button/radio-button.component';
 import { NgModelGroupComponent } from './elements/ng-model-group/ng-model-group.component';
-import { FormProfessionComponent } from './elements/form-profession/form-profession.component';
+import { FormProfessionComponent } from './elements/form-with-children/form-profession/form-profession.component';
 
 @NgModule({
   declarations: [
