71e1bda229ecc99d1d179b53050ae2fbb3c004da
j0jds <mrlucassilva7342@gmail.com>
2024-10-02 13:55:34 -0300

    Testing input types and methods

--- Angular/Templates/Template Driven-forms/Driven-Forms/src/app/elements/form/form.component.ts
+++ Angular/Templates/Template Driven-forms/Driven-Forms/src/app/elements/form/form.component.ts
@@ -1,17 +1,33 @@
-import { Component, AfterViewInit, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
+
 import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
-  styleUrl: './form.component.scss'
+  styleUrl: './form.component.scss',
 })
 
-export class FormComponent implements AfterViewInit {
-  @ViewChild('meuForm') form!: NgForm;
+export class FormComponent {
+  inputText: string = 'SPLASH';
+  textarea: string = 'SPLASH';
+  select: string = '1';
+  radioButton: string = '1';
+  checkbox: boolean = true;
 
-  ngAfterViewInit() {
-    console.log(this.form);
+  onSubmit(form: NgForm) {
+    console.log('onSubmit', form.value);
+  }  
+
+  onReset(form: NgForm) {
+    console.log(this.select)
+    console.log(this.checkbox)
+    console.log(this.textarea)
+    console.log(this.inputText)
+    console.log(this.radioButton)
+    console.log('onReset', form.value);
+
+    setTimeout(() => console.log(form.value), 2000)
   }
 
 }
