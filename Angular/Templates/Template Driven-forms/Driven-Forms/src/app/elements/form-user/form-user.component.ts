f41292bf215851247c3a1d1ee0d2076c01dc0f9c
j0jds <mrlucassilva7342@gmail.com>
2024-10-02 17:08:58 -0300

    General update in HTML files and more

--- Angular/Templates/Template Driven-forms/Driven-Forms/src/app/elements/form-user/form-user.component.ts
+++ Angular/Templates/Template Driven-forms/Driven-Forms/src/app/elements/form-user/form-user.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { ControlContainer, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-form-user',
   templateUrl: './form-user.component.html',
-  styleUrl: './form-user.component.scss'
+  styleUrl: './form-user.component.scss',
+  viewProviders: [{ provide: ControlContainer, useExisting: NgForm }],
 })
+
 export class FormUserComponent {
 
 }
