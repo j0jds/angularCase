80ad66f476b2261162f097c494e33b95ab1435a1
j0jds <mrlucassilva7342@gmail.com>
2024-10-03 12:24:11 -0300

    Changing folders names and realocating files

--- Angular/Templates/Template Driven-forms/src/app/app.module.ts
+++ Angular/Templates/Template Driven-forms/src/app/app.module.ts
@@ -6,13 +6,14 @@ import { FormComponent } from './elements/form/form.component';
 import { SelectComponent } from './elements/select/select.component';
 import { AppRoutingModule } from './app-routing.module';
 import { CheckboxComponent } from './elements/checkbox/checkbox.component';
-import { FormUserComponent } from './elements/form-with-children/form-user/form-user.component';
-import { MainFormComponent } from './elements/form-with-children/main-form/main-form.component';
+import { FormUserComponent } from './form-with-children/form-user/form-user.component';
+import { MainFormComponent } from './form-with-children/main-form/main-form.component';
 import { TextareaComponent } from './elements/textarea/textarea.component';
 import { InputTextComponent } from './elements/input-text/input-text.component';
 import { RadioButtonComponent } from './elements/radio-button/radio-button.component';
 import { NgModelGroupComponent } from './elements/ng-model-group/ng-model-group.component';
-import { FormProfessionComponent } from './elements/form-with-children/form-profession/form-profession.component';
+import { FormProfessionComponent } from './form-with-children/form-profession/form-profession.component';
+import { CustomValidatorFormEx1Component } from './custom-validators/synchronous/ex1/custom-validator-form-ex1/custom-validator-form-ex1.component';
 
 @NgModule({
   declarations: [
@@ -27,6 +28,7 @@ import { FormProfessionComponent } from './elements/form-with-children/form-prof
     RadioButtonComponent,
     NgModelGroupComponent,
     FormProfessionComponent,
+    CustomValidatorFormEx1Component,
   ],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
