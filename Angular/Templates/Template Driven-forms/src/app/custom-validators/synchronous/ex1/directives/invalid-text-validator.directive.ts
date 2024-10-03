3427ace49e5faa62d8a1b42b72a512eafe895f3e
j0jds <mrlucassilva7342@gmail.com>
2024-10-03 13:02:14 -0300

    Added custom validator for input

--- Angular/Templates/Template Driven-forms/src/app/custom-validators/synchronous/ex1/directives/invalid-text-validator.directive.ts
+++ Angular/Templates/Template Driven-forms/src/app/custom-validators/synchronous/ex1/directives/invalid-text-validator.directive.ts
@@ -1,11 +1,21 @@
 import { Directive } from '@angular/core';
+import { AbstractControl, NG_VALIDATORS, ValidationErrors, Validator } from '@angular/forms';
 
 @Directive({
-  selector: '[appInvalidTextValidator]'
+  selector: '[appInvalidTextValidator]',
+  providers: [
+    {
+      provide: NG_VALIDATORS,
+      useExisting: InvalidTextValidatorDirective,
+      multi: true,
+    },
+  ],
 })
 
-export class InvalidTextValidatorDirective {
+export class InvalidTextValidatorDirective implements Validator {
+  validate(control: AbstractControl<any, any>): ValidationErrors | null {
+    const isValid = !control.value.includes('joel');
 
-  constructor() { }
-
+    return isValid ? null : { invalidText: 'true' };
+  }
 }
