bb5728b08b88193678dec8539bbaf2dd8327c5f4
j0jds <mrlucassilva7342@gmail.com>
2024-10-03 14:35:35 -0300

    Making a HTTP request using HttpClient

---
+++ Angular/Templates/Template Driven-forms/src/app/custom-validators/asynchronous/service/users.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class UsersService {
+
+  constructor(
+    private readonly _httpClient: HttpClient
+  ) {}
+
+  getUsers(): Observable<{ name: string }[]> {
+     return this._httpClient.get<{ name: string }[]>('https://jsonplaceholder.typicode.com/users')
+  }
+
+
+}
