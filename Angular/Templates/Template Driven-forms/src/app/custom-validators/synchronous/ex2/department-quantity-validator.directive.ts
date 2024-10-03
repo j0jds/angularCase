3447deea28e674d29d9e968b9096f263c15328b0
j0jds <mrlucassilva7342@gmail.com>
2024-10-03 14:14:31 -0300

    Added conditions for exhibit things

--- Angular/Templates/Template Driven-forms/src/app/custom-validators/synchronous/ex2/department-quantity-validator.directive.ts
+++ Angular/Templates/Template Driven-forms/src/app/custom-validators/synchronous/ex2/department-quantity-validator.directive.ts
@@ -14,7 +14,14 @@ import { AbstractControl, NG_VALIDATORS, ValidationErrors, Validator } from '@an
 
 export class DepartmentQuantityValidatorDirective implements Validator {
   validate(control: AbstractControl<any, any>): ValidationErrors | null {
-    console.log('control', control);
+
+    if (control.value.departamento === 'IT' && +control.value.quantidade > 10) {
+      return { 'invalidITQuantity': 'true' }
+    }
+
+    if (control.value.departamento === 'HR' && +control.value.quantidade > 20) {
+      return { 'invalidHRQuantity': 'true' }
+    }
 
     return null;
   }
