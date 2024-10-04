7e1922ed48f835fe1f250bfbb69a0b12c37772c7
j0jds <mrlucassilva7342@gmail.com>
2024-10-04 17:20:03 -0300

    Configuration for email validation directive

--- Projects/Template Driven Forms/Examples/src/app/components/ex2/directives/email-validator.directive.ts
+++ Projects/Template Driven Forms/Examples/src/app/components/ex2/directives/email-validator.directive.ts
@@ -1,10 +1,23 @@
 import { Directive } from '@angular/core';
+import { AbstractControl, NG_VALIDATORS, ValidationErrors, Validator } from '@angular/forms';
 
 @Directive({
-  selector: '[appEmailValidator]'
+  selector: '[appEmailValidator]',
+  providers: [
+    {
+      provide: NG_VALIDATORS,
+      useExisting: EmailValidatorDirective,
+      multi: true,
+    },
+  ],
 })
-export class EmailValidatorDirective {
 
-  constructor() { }
+export class EmailValidatorDirective implements Validator {
+  validate(control: AbstractControl<any, any>): ValidationErrors | null {
+    const emailpattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
+    const IS_VALID = emailpattern.test(control.value);
+
+    return IS_VALID ? null : { invalidEmail: true };
+  }
 }
