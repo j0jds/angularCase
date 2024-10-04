b942450f093f45d7d88ef7fbf8accafeb36ccf15
j0jds <mrlucassilva7342@gmail.com>
2024-10-04 15:46:26 -0300

    Added initial examples of angular material elemenets

--- Projects/Template Driven Forms/Examples/src/app/app.component.ts
+++ Projects/Template Driven Forms/Examples/src/app/app.component.ts
@@ -5,6 +5,5 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'Examples';
-}
+
+export class AppComponent {}
