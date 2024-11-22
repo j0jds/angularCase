0c4ccc192062bc1f511f703a5ec8622090762758
j0jds <mrlucassilva7342@gmail.com>
2024-11-22 17:03:42 -0300

    Created form control component

--- Angular/Reactive Forms/testCase/src/app/app.module.ts
+++ Angular/Reactive Forms/testCase/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormControlComponent } from './examplesCase/form-control/form-control.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormControlComponent
   ],
   imports: [
     BrowserModule,
