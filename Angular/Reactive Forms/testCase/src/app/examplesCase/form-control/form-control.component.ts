9073dc005c2ad7042924bea19e3b63cab5851c4c
j0jds <mrlucassilva7342@gmail.com>
2024-11-22 17:49:37 -0300

    Added changes (incomplete)

--- Angular/Reactive Forms/testCase/src/app/examplesCase/form-control/form-control.component.ts
+++ Angular/Reactive Forms/testCase/src/app/examplesCase/form-control/form-control.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-form-control',
   templateUrl: './form-control.component.html',
   styleUrl: './form-control.component.scss'
 })
-export class FormControlComponent {
 
+export class FormControlComponent {
+  nome = new FormControl('') 
 }
