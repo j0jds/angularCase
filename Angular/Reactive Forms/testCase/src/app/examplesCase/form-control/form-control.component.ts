c2871cf086b7dee7bf94007d2a54878ed2e08d5a
j0jds <mrlucassilva7342@gmail.com>
2024-11-29 17:39:53 -0300

    Added valueChanges and more comments

--- Angular/Reactive Forms/testCase/src/app/examplesCase/form-control/form-control.component.ts
+++ Angular/Reactive Forms/testCase/src/app/examplesCase/form-control/form-control.component.ts
@@ -12,24 +12,26 @@ export class FormControlComponent implements OnInit {
   // nome = new FormControl('Inicial', [Validators.required])
 
   // nome = new FormControl('Inicial', { nonNullable: true, validators: [Validators.required]});
-  // Aqui o FormControl recebe um nonNullable que faz com que ele seja válido ainda que seja untouched e não seja dirty.
-  // Ao abrir um [] torna-se possível passar mais de um validador para o validators, enquanto nesse caso foi passado apenas um que é o required.
+  /* Aqui o FormControl recebe um nonNullable que faz com que ele seja válido ainda que seja untouched e não seja dirty. Ao abrir um [] torna-se possível passar mais de um validador para o validators, enquanto nesse caso foi passado apenas um que é o required. **/
   
   nome = new FormControl('Inicial', [Validators.required, Validators.minLength(6)])
   // Aqui o FormControl recebe um validators de minLength com pelo menos 6 caracteres. Se tiver menos que 6, então ele retorna falso.
 
   ngOnInit() {
     console.log(this.nome);
+
+    this.nome.valueChanges.subscribe(result => console.log('valueChanges: ', result));
   }
 
   mostrarStatus() {
     console.log(this.nome);
   }
 
+  /* Aqui o setValue altera a propriedade value de nome. É interessante notar como funciona o acesso de cada propriedade específica e como eles modificam o comportamento de cada uma delas. **/
   alterarValor() {
     this.nome.setValue('Z');
   }
-
+  
   inputAlterado() {
     console.log(this.nome.value);
   }  
