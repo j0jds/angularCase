937afffe94217af2cdfa70f29cb9e2444f7c4196
j0jds <mrlucassilva7342@gmail.com>
2024-12-03 17:47:41 -0300

    Changing validators settings (wip)

--- Angular/Reactive Forms/testCase/src/app/examplesCase/form-control/form-control.component.ts
+++ Angular/Reactive Forms/testCase/src/app/examplesCase/form-control/form-control.component.ts
@@ -13,10 +13,14 @@ export class FormControlComponent implements OnInit {
   // nome = new FormControl('Inicial', { nonNullable: true, validators: [Validators.required]});
   /* Aqui o FormControl recebe um nonNullable que faz com que ele seja válido ainda que seja untouched e não seja dirty. Ao abrir um [] torna-se possível passar mais de um validador para o validators, enquanto nesse caso foi passado apenas um que é o required. */
 
-  nome = new FormControl('Inicial', [Validators.required, Validators.minLength(6)]);
+  // nome = new FormControl('Inicial', [Validators.required, Validators.minLength(6)]);
   // Aqui o FormControl recebe um validators de minLength com pelo menos 6 caracteres. Se tiver menos que 6, então ele retorna falso.
 
-  // nome = new FormControl('Inicial', {nonNullable: true, validators: [Validators.required], updateOn: "blur"});
+  nome = new FormControl('Inicial', {
+    nonNullable: true,
+    validators: [Validators.required, Validators.minLength(6), Validators.maxLength(7)],
+    // updateOn: "change"
+  });
 
   audio = new Audio('assets/sound.mp3');
   
