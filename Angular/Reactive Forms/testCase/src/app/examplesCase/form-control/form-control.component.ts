9b07f21992a20c17c7579d41ec7d92c3b18a5e41
j0jds <mrlucassilva7342@gmail.com>
2024-12-03 14:28:49 -0300

    Added setValidators and addValidators methods with comments

--- Angular/Reactive Forms/testCase/src/app/examplesCase/form-control/form-control.component.ts
+++ Angular/Reactive Forms/testCase/src/app/examplesCase/form-control/form-control.component.ts
@@ -12,7 +12,7 @@ export class FormControlComponent implements OnInit {
   // nome = new FormControl('Inicial', [Validators.required])
 
   // nome = new FormControl('Inicial', { nonNullable: true, validators: [Validators.required]});
-  /* Aqui o FormControl recebe um nonNullable que faz com que ele seja válido ainda que seja untouched e não seja dirty. Ao abrir um [] torna-se possível passar mais de um validador para o validators, enquanto nesse caso foi passado apenas um que é o required. **/
+  /* Aqui o FormControl recebe um nonNullable que faz com que ele seja válido ainda que seja untouched e não seja dirty. Ao abrir um [] torna-se possível passar mais de um validador para o validators, enquanto nesse caso foi passado apenas um que é o required. */
   
   nome = new FormControl('Inicial', [Validators.required, Validators.minLength(6)]);
   // Aqui o FormControl recebe um validators de minLength com pelo menos 6 caracteres. Se tiver menos que 6, então ele retorna falso.
@@ -30,7 +30,7 @@ export class FormControlComponent implements OnInit {
     console.log(this.nome);
   }
 
-  /* Aqui o setValue altera a propriedade value de nome. É interessante notar como funciona o acesso de cada propriedade específica e como eles modificam o comportamento de cada uma delas. **/
+  /* Aqui o setValue altera a propriedade value de nome. É interessante notar como funciona o acesso de cada propriedade específica e como eles modificam o comportamento de cada uma delas. */
   alterarValor() {
     this.nome.setValue('Z');
   }
@@ -53,5 +53,21 @@ export class FormControlComponent implements OnInit {
 
   limparConsole() {
     console.clear();
+  }
+
+  // Esses métodos foram adicionados com o intuito de sobrescrever os validadores já estabelecidos lá em cima.
+
+  /* O que acontece no setValidators: o método zera os validadores, adiciona um valor inicial de 2 caracteres, substitui o validador anterior pelo de tamanho mínimo (6) e o último método aplica as mudanças de imediato e reavalia o estado do control. */
+  setValidators() {
+    this.nome.setValidators([]);
+    this.nome.setValue('Ep');
+    this.nome.setValidators(Validators.minLength(6));
+    this.nome.updateValueAndValidity();
+  }
+
+  /* O que acontece em addValidators: o método adiciona o validador de tamanho máximo a nome e em seguida aplica as mudanças de imediato e reavalia o estado do control. */
+  addValidators() {
+    this.nome.addValidators(Validators.maxLength(7));
+    this.nome.updateValueAndValidity();
   }
 }
