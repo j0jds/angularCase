9e9c71e7ae6ac8d49b996dd31f4ff71d909c2841
j0jds <mrlucassilva7342@gmail.com>
2024-12-05 14:35:46 -0300

    Changing property and more

--- Angular/Reactive Forms/testCase/src/app/examplesCase/form-control/form-control.component.ts
+++ Angular/Reactive Forms/testCase/src/app/examplesCase/form-control/form-control.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
+import { invalidTextValidator } from './invalid-text-validator';
 
 @Component({
   selector: 'app-form-control',
@@ -8,7 +9,7 @@ import { FormControl, Validators } from '@angular/forms';
 })
 export class FormControlComponent implements OnInit {
 
-  // nome = new FormControl('Inicial', [Validators.required])
+  nome = new FormControl('Inicial', [invalidTextValidator('epilef')]);
 
   // nome = new FormControl('Inicial', { nonNullable: true, validators: [Validators.required]});
   /* Aqui o FormControl recebe um nonNullable que faz com que ele seja válido ainda que seja untouched e não seja dirty. Ao abrir um [] torna-se possível passar mais de um validador para o validators, enquanto nesse caso foi passado apenas um que é o required. */
@@ -16,11 +17,11 @@ export class FormControlComponent implements OnInit {
   // nome = new FormControl('Inicial', [Validators.required, Validators.minLength(6)]);
   // Aqui o FormControl recebe um validators de minLength com pelo menos 6 caracteres. Se tiver menos que 6, então ele retorna falso.
 
-  nome = new FormControl('Inicial', {
-    nonNullable: true,
-    validators: [Validators.required, Validators.minLength(6), Validators.maxLength(7)],
-    // updateOn: "change"
-  });
+  // nome = new FormControl('Inicial', {
+  //   nonNullable: true,
+  //   validators: [Validators.required, Validators.minLength(6), Validators.maxLength(7)],
+  //   // updateOn: "change"
+  // });
 
   audio = new Audio('assets/sound.mp3');
   
