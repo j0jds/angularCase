743156304b12a781bf82172aaf5d100df4c0e2b5
j0jds <mrlucassilva7342@gmail.com>
2024-12-05 15:59:05 -0300

    Create asynchronous validator component

---
+++ Angular/Reactive Forms/testCase/src/app/examplesCase/asynchronous-validator/asynchronous-validator.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-asynchronous-validator',
+  templateUrl: './asynchronous-validator.component.html',
+  styleUrl: './asynchronous-validator.component.scss'
+})
+
+export class AsynchronousValidatorComponent {
+
+}
