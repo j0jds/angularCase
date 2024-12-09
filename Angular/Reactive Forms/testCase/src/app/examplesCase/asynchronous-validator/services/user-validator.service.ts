feac2fae55994a27998dd5beaa494905f806d371
j0jds <mrlucassilva7342@gmail.com>
2024-12-09 17:47:19 -0300

    Minor changes in service (wip)

--- Angular/Reactive Forms/testCase/src/app/examplesCase/asynchronous-validator/services/user-validator.service.ts
+++ Angular/Reactive Forms/testCase/src/app/examplesCase/asynchronous-validator/services/user-validator.service.ts
@@ -1,13 +1,17 @@
 import { Injectable } from "@angular/core";
 import { AbstractControl, AsyncValidator, ValidationErrors } from "@angular/forms";
 import { Observable } from "rxjs";
+import { UsersService } from "./users.service";
 
 @Injectable ({
     providedIn: 'root'
 })
 
 export class UserValidatorService implements AsyncValidator {
+    constructor(
+        private readonly _usersService: UsersService
+    ){}
     validate(control: AbstractControl<any, any>): Promise<ValidationErrors | null> | Observable<ValidationErrors | null> {
-        throw new Error("Method not implemented.");
+        return this._usersService.getUsers();
     }
 }
