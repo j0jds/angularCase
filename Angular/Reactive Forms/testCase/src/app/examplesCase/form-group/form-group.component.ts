649968ecf77da75c49e56cb7e895edf17a857bed
j0jds <mrlucassilva7342@gmail.com>
2024-12-12 12:04:41 -0300

    Create form group component

---
+++ Angular/Reactive Forms/testCase/src/app/examplesCase/form-group/form-group.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-group',
+  templateUrl: './form-group.component.html',
+  styleUrl: './form-group.component.scss'
+})
+
+export class FormGroupComponent {}
