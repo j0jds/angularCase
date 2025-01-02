a58edf1b2a80a740beed691f77e51567a95fd43d
j0jds <mrlucassilva7342@gmail.com>
2025-01-02 15:11:46 -0300

    Added imports and create musicasForm

--- Angular/Reactive Forms/testCase/src/app/examplesCase/form-array/form-array.component.ts
+++ Angular/Reactive Forms/testCase/src/app/examplesCase/form-array/form-array.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form-array',
   templateUrl: './form-array.component.html',
   styleUrl: './form-array.component.scss'
 })
+
 export class FormArrayComponent {
+
+  musicasForm = new FormGroup({
+    musicas: new FormArray([
+      new FormControl('', [Validators.required])
+    ]),
+  })
+
+  constructor() {
+    console.log(this.musicasForm);
+  }
 
 }
