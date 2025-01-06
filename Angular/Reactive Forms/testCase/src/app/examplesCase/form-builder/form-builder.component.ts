95feb3e8626794c9e7e4ebd2f23df3b0c0c51d06
j0jds <mrlucassilva7342@gmail.com>
2025-01-06 12:58:19 -0300

    Added logic to form builder component

--- Angular/Reactive Forms/testCase/src/app/examplesCase/form-builder/form-builder.component.ts
+++ Angular/Reactive Forms/testCase/src/app/examplesCase/form-builder/form-builder.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form-builder',
   templateUrl: './form-builder.component.html',
   styleUrl: './form-builder.component.scss'
 })
-export class FormBuilderComponent {
+
+export class FormBuilderComponent implements OnInit {
+
+  pessoaForm!: FormGroup; 
+
+  // O construtor é utilizado para injetar dependências necessárias no componente.
+  constructor(
+    private readonly _fb: FormBuilder
+  ) { }
+
+  // O formulário é inicializado no ngOnInit para garantir que esteja sincronizado com o ciclo de vida do componente.
+  ngOnInit() {
+    this.pessoaForm = this._fb.group({
+      nome: ['', [Validators.required]], 
+      email: this._fb.control('', { validators: [Validators.required] }), 
+      endereco: this._fb.group({ 
+        rua: ['', [Validators.required]], 
+        numero: ['', [Validators.required]] 
+      }),
+      musicas: this._fb.array([ 
+        ['', [Validators.required]],
+      ])
+    })
+  }
 
 }
