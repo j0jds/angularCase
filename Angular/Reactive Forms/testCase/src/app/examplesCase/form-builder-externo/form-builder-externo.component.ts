dbc11f70c2c56e5e41519573d87d2be3eba4ec52
j0jds <mrlucassilva7342@gmail.com>
2025-01-06 16:18:09 -0300

    Added method for submit

--- Angular/Reactive Forms/testCase/src/app/examplesCase/form-builder-externo/form-builder-externo.component.ts
+++ Angular/Reactive Forms/testCase/src/app/examplesCase/form-builder-externo/form-builder-externo.component.ts
@@ -1,19 +1,24 @@
-import { Component } from '@angular/core';
 import { PessoaFormController } from './pessoa-form-controller';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-form-builder-externo',
   templateUrl: './form-builder-externo.component.html',
-  styleUrl: './form-builder-externo.component.scss'
+  styleUrl: './form-builder-externo.component.scss',
 })
 
 // Epilef recomendou evitar criar uma classe pra estender a classe principal do seu componente, pois isso torna a manutenção mais difícil. Ele recomenda que, quando necessário, você deve estender a classe que possui uma única funcionalidade e não mais que isso.
 export class FormBuilderExternoComponent extends PessoaFormController {
+  @Output('onFormSubmit') onFormSubmitEmit = new EventEmitter();
 
-  constructor(
-    private readonly _fbMain: FormBuilder
-  ) {
+  constructor(private readonly _fbMain: FormBuilder) {
     super(_fbMain);
+  }
+
+  onFormSubmit() {
+    console.log('onFormSubmit =>', this.pessoaForm.value);
+
+    this.onFormSubmitEmit.emit(this.pessoaForm.value);
   }
 }
