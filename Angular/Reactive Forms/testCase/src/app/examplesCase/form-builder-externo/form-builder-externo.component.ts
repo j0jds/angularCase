faa8b131870195689c7667bcb8f58b2cd5de7d24
j0jds <mrlucassilva7342@gmail.com>
2025-01-06 13:40:43 -0300

    Added comment and minor changes

--- Angular/Reactive Forms/testCase/src/app/examplesCase/form-builder-externo/form-builder-externo.component.ts
+++ Angular/Reactive Forms/testCase/src/app/examplesCase/form-builder-externo/form-builder-externo.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { PessoaFormController } from './pessoa-form-controller';
 
 @Component({
   selector: 'app-form-builder-externo',
   templateUrl: './form-builder-externo.component.html',
   styleUrl: './form-builder-externo.component.scss'
 })
-export class FormBuilderExternoComponent {
+
+// Epilef recomendou evitar criar uma classe pra estender a classe principal do seu componente, pois isso torna a manutenção mais difícil. Ele recomenda que, quando necessário, você deve estender a classe que possui uma única funcionalidade e não mais que isso.
+export class FormBuilderExternoComponent extends PessoaFormController {
+  
 
 }
