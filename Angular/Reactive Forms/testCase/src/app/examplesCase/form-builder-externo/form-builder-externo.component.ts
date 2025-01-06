b9c76291490d9ba9e49b7e5718d6b01769c1b21f
j0jds <mrlucassilva7342@gmail.com>
2025-01-06 14:19:38 -0300

    Added constructor and reference in form builder component

--- Angular/Reactive Forms/testCase/src/app/examplesCase/form-builder-externo/form-builder-externo.component.ts
+++ Angular/Reactive Forms/testCase/src/app/examplesCase/form-builder-externo/form-builder-externo.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { PessoaFormController } from './pessoa-form-controller';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-form-builder-externo',
@@ -9,6 +10,10 @@ import { PessoaFormController } from './pessoa-form-controller';
 
 // Epilef recomendou evitar criar uma classe pra estender a classe principal do seu componente, pois isso torna a manutenção mais difícil. Ele recomenda que, quando necessário, você deve estender a classe que possui uma única funcionalidade e não mais que isso.
 export class FormBuilderExternoComponent extends PessoaFormController {
-  
 
+  constructor(
+    private readonly _fbMain: FormBuilder
+  ) {
+    super(_fbMain);
+  }
 }
