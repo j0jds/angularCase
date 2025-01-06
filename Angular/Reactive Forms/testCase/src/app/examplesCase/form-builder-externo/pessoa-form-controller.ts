b75e91541f4a79c50aab42d7b956ddca3cbe745b
j0jds <mrlucassilva7342@gmail.com>
2025-01-06 14:19:16 -0300

    Added constructor in controller

--- Angular/Reactive Forms/testCase/src/app/examplesCase/form-builder-externo/pessoa-form-controller.ts
+++ Angular/Reactive Forms/testCase/src/app/examplesCase/form-builder-externo/pessoa-form-controller.ts
@@ -1,5 +1,21 @@
-import { FormGroup } from "@angular/forms";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 export class PessoaFormController {
     pessoaForm!: FormGroup;
+
+    constructor(
+        private readonly _fb: FormBuilder
+    ) {
+        this.pessoaForm = this._fb.group({
+            nome: ['', [Validators.required]], 
+            email: this._fb.control('', { validators: [Validators.required] }), 
+            endereco: this._fb.group({ 
+              rua: ['', [Validators.required]], 
+              numero: ['', [Validators.required]] 
+            }),
+            musicas: this._fb.array([ 
+              ['', [Validators.required]],
+            ])
+          })
+    }
 }
