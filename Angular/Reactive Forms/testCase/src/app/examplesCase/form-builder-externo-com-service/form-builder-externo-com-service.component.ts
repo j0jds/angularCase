35dd808ab47a368d471ab41f7490502bf40ceda3
j0jds <mrlucassilva7342@gmail.com>
2025-01-07 14:11:26 -0300

    Added service reference

--- Angular/Reactive Forms/testCase/src/app/examplesCase/form-builder-externo-com-service/form-builder-externo-com-service.component.ts
+++ Angular/Reactive Forms/testCase/src/app/examplesCase/form-builder-externo-com-service/form-builder-externo-com-service.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { PessoaFormControllerService } from './pessoa-form-controller.service';
 
 @Component({
   selector: 'app-form-builder-externo-com-service',
   templateUrl: './form-builder-externo-com-service.component.html',
   styleUrl: './form-builder-externo-com-service.component.scss'
 })
+
 export class FormBuilderExternoComServiceComponent {
+  constructor(
+    private readonly _pessoaFormControllerService: PessoaFormControllerService
+  ) {}
 
 }
