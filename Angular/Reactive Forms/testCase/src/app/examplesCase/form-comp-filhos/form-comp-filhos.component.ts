c4f5b65784d5d20ad08b0d507b64c4b1ca594d21
j0jds <mrlucassilva7342@gmail.com>
2025-01-10 17:08:35 -0300

    Create new component

---
+++ Angular/Reactive Forms/testCase/src/app/examplesCase/form-comp-filhos/form-comp-filhos.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-comp-filhos',
+  templateUrl: './form-comp-filhos.component.html',
+  styleUrl: './form-comp-filhos.component.scss'
+})
+
+export class FormCompFilhosComponent {
+
+}
