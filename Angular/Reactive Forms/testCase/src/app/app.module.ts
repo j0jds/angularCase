0a7616a249a540d3ede19a6b868b5b9d5cca38bf
j0jds <mrlucassilva7342@gmail.com>
2025-01-10 17:08:46 -0300

    Minor changes in app module

--- Angular/Reactive Forms/testCase/src/app/app.module.ts
+++ Angular/Reactive Forms/testCase/src/app/app.module.ts
@@ -1,30 +1,31 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FormControlComponent } from './examplesCase/form-control/form-control.component';
-import { ReactiveFormsModule } from '@angular/forms';
-// import { ValidadorAssincronoComponent } from './examplesCase/validador-assincrono/validador-assincrono.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 import { FormGroupComponent } from './examplesCase/form-group/form-group.component';
 import { FormArrayComponent } from './examplesCase/form-array/form-array.component';
-import { FormArrayWithGroupComponent } from './examplesCase/form-array-with-group/form-array-with-group.component';
+import { ReactiveFormsModule } from '@angular/forms';
 import { FormBuilderComponent } from './examplesCase/form-builder/form-builder.component';
+import { FormControlComponent } from './examplesCase/form-control/form-control.component';
+import { FormCompFilhosComponent } from './examplesCase/form-comp-filhos/form-comp-filhos.component';
+import { FormArrayWithGroupComponent } from './examplesCase/form-array-with-group/form-array-with-group.component';
 import { FormBuilderExternoComponent } from './examplesCase/form-builder-externo/form-builder-externo.component';
+import { ValidadorAssincronoComponent } from './examplesCase/validador-assincrono/validador-assincrono.component';
 import { FormBuilderExternoComServiceComponent } from './examplesCase/form-builder-externo-com-service/form-builder-externo-com-service.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FormControlComponent,
-    // ValidadorAssincronoComponent,
+    ValidadorAssincronoComponent,
     FormGroupComponent,
     FormArrayComponent,
     FormArrayWithGroupComponent,
     FormBuilderComponent,
     FormBuilderExternoComponent,
-    FormBuilderExternoComServiceComponent
+    FormBuilderExternoComServiceComponent,
+    FormCompFilhosComponent
   ],
   imports: [
     BrowserModule,
