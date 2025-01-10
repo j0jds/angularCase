b0cf209a3590da65b85d5ea0fb5c7b205528645e
j0jds <mrlucassilva7342@gmail.com>
2025-01-10 17:31:08 -0300

    Added folder and new component

--- Angular/Reactive Forms/testCase/src/app/app.module.ts
+++ Angular/Reactive Forms/testCase/src/app/app.module.ts
@@ -3,12 +3,13 @@ import { AppComponent } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
+import { EnderecoComponent } from './examplesCase/form-comp-filhos/components/endereco/endereco.component';0
 import { FormGroupComponent } from './examplesCase/form-group/form-group.component';
 import { FormArrayComponent } from './examplesCase/form-array/form-array.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { FormBuilderComponent } from './examplesCase/form-builder/form-builder.component';
 import { FormControlComponent } from './examplesCase/form-control/form-control.component';
-import { FormCompFilhosComponent } from './examplesCase/form-comp-filhos/form-comp-filhos.component';
+import { FormCompFilhosComponent } from './examplesCase/form-comp-filhos/components/form-comp-filhos.component';
 import { FormArrayWithGroupComponent } from './examplesCase/form-array-with-group/form-array-with-group.component';
 import { FormBuilderExternoComponent } from './examplesCase/form-builder-externo/form-builder-externo.component';
 import { ValidadorAssincronoComponent } from './examplesCase/validador-assincrono/validador-assincrono.component';
@@ -25,7 +26,8 @@ import { FormBuilderExternoComServiceComponent } from './examplesCase/form-build
     FormBuilderComponent,
     FormBuilderExternoComponent,
     FormBuilderExternoComServiceComponent,
-    FormCompFilhosComponent
+    FormCompFilhosComponent,
+    EnderecoComponent
   ],
   imports: [
     BrowserModule,
---
+++ Angular/Reactive Forms/testCase/src/app/examplesCase/form-comp-filhos/components/endereco/endereco.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-endereco',
+  templateUrl: './endereco.component.html',
+  styleUrl: './endereco.component.scss'
+})
+
+export class EnderecoComponent {
+
+}
