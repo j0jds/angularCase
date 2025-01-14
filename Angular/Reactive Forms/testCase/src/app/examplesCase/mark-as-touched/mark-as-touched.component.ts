bddc3eb369ee7eac71b3fcc518069e57f7eeecf6
j0jds <mrlucassilva7342@gmail.com>
2025-01-14 14:34:02 -0300

    Create a custom validator

--- Angular/Reactive Forms/testCase/src/app/examplesCase/mark-as-touched/mark-as-touched.component.ts
+++ Angular/Reactive Forms/testCase/src/app/examplesCase/mark-as-touched/mark-as-touched.component.ts
@@ -1,6 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormControl, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 
+// Criação de um validador personalizado. Cabem diferentes tipos de testes para ver maneiras diferentes de como isso pode funcionar.
+function testeValidator(): ValidatorFn {
+  return (): ValidationErrors | null => {
+    console.log('VALIDADOR!!!')
+    return null
+  }
+}
 @Component({
   selector: 'app-mark-as-touched',
   templateUrl: './mark-as-touched.component.html',
@@ -8,14 +15,14 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 
 export class MarkAsTouchedComponent implements OnInit {
-
-
   pessoaForm = new FormGroup({
-    nome: new FormControl('', Validators.required),
-    email: new FormControl('', Validators.required)
+    nome: new FormControl('', [Validators.required, testeValidator()]),
+    email: new FormControl('', [Validators.required])
   })
 
   ngOnInit(): void {
+    this.pessoaForm.get('nome')?.statusChanges.subscribe((value) => console.log('Status Changes Nome =>', value))
+
     setTimeout(() => {
       console.log('Touched pessoaForm', this.pessoaForm.touched)
       console.log('Touched nomeControl', this.pessoaForm.get('nome')?.touched);
@@ -25,8 +32,8 @@ export class MarkAsTouchedComponent implements OnInit {
 
   markNomeAsTouched() {
     console.log('markNomeAsTouched');
-    this.pessoaForm.get('nome')?.markAsTouched({ onlySelf: true });
+    // this.pessoaForm.get('nome')?.markAsTouched({ onlySelf: true });
+    this.pessoaForm.get('nome')?.markAsTouched();
     // this.pessoaForm.get('email')?.markAsTouched();
   }
-
 }
