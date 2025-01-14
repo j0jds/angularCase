b16c3ef407b8145a05dbe83421940c83503b7a9c
j0jds <mrlucassilva7342@gmail.com>
2025-01-14 14:08:44 -0300

    Added methods for verification

--- Angular/Reactive Forms/testCase/src/app/examplesCase/mark-as-touched/mark-as-touched.component.ts
+++ Angular/Reactive Forms/testCase/src/app/examplesCase/mark-as-touched/mark-as-touched.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-mark-as-touched',
   templateUrl: './mark-as-touched.component.html',
   styleUrl: './mark-as-touched.component.scss'
 })
-export class MarkAsTouchedComponent {
+
+export class MarkAsTouchedComponent implements OnInit {
+
+
+  pessoaForm = new FormGroup({
+    nome: new FormControl('', Validators.required),
+    email: new FormControl('', Validators.required)
+  })
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      console.log('Touched pessoaForm', this.pessoaForm.touched)
+      console.log('Touched nomeControl', this.pessoaForm.get('nome')?.touched);
+      console.log('Touched emailControl', this.pessoaForm.get('email')?.touched);
+    }, 1000)
+  }
+
+  markNomeAsTouched() {
+    console.log('markNomeAsTouched');
+    this.pessoaForm.get('nome')?.markAsTouched();
+    this.pessoaForm.get('email')?.markAsTouched();
+  }
 
 }
