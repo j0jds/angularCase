ce171dc33ffe974d691c1e77db71febcf4cdb5e3
j0jds <mrlucassilva7342@gmail.com>
2025-01-14 17:37:35 -0300

    Added settings to form

--- Angular/Reactive Forms/testCase/src/app/exercicio-mat/exercicio-mat.component.ts
+++ Angular/Reactive Forms/testCase/src/app/exercicio-mat/exercicio-mat.component.ts
@@ -4,14 +4,13 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-exercicio-mat',
   templateUrl: './exercicio-mat.component.html',
-  styleUrl: './exercicio-mat.component.scss'
+  styleUrl: './exercicio-mat.component.scss',
 })
 
 export class ExercicioMatComponent {
-
   pessoaForm = new FormGroup({
-    nome: new FormControl ('', Validators.required),
-    email: new FormControl ('', Validators.required),
+    nome: new FormControl('', Validators.required),
+    email: new FormControl('', Validators.required),
   });
 
   get nome(): FormControl {
@@ -22,4 +21,18 @@ export class ExercicioMatComponent {
     return this.pessoaForm.get('email') as FormControl;
   }
 
+  onFormSubmit() {
+    if (this.pessoaForm.invalid) {
+      console.log('Formulário Invállido!');
+      // this.pessoaForm.markAllAsTouched
+      Object.keys(this.pessoaForm.controls).forEach((controlKey) => {
+        const control = this.pessoaForm.get(controlKey);
+        if (control?.invalid) {
+          control.markAsTouched();
+        }
+      });
+    } else {
+      console.log('Formulário Enviado! =>', this.pessoaForm.value);
+    }
+  }
 }
